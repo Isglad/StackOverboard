8d2672097d9c28b0cab45f259ce3529cbf0f74a7
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2024-11-19 14:09:36 -0800

    User Test Fixture creation

---
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -0,0 +1,21 @@
+package com.teamtreehouse.techdegree.overboard.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    private User user;
+    private Board board;
+    private Question question;
+    private Answer answer;
+
+    @Before
+    public void setUp() throws Exception {
+       // user = new User();
+      //  board = new Board();
+     //   question = new Question();
+     //   answer = new Answer();
+    }
+}
