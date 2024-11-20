3897edd32502973d268ba44d7c3c4a2a224f95d8
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2024-11-20 13:52:58 -0800

    tests that authors cannot vote either up or down

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -1,7 +1,11 @@
 package com.teamtreehouse.techdegree.overboard.model;
 
+import com.teamtreehouse.techdegree.overboard.exc.AnswerAcceptanceException;
+import com.teamtreehouse.techdegree.overboard.exc.VotingException;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +14,12 @@ public class UserTest {
     private Board board;
     private User user1;
     private User user2;
+    private User user3;
     private Question question;
     public Answer answer;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
@@ -21,6 +29,7 @@ public class UserTest {
         // Initialize Users
         user1 = board.createUser("Jane");
         user2 = board.createUser("Joe");
+        user3 = board.createUser("Doe");
 
         // Initialize a Question
         question = user1.askQuestion("What is a Test Fixture?");
@@ -60,11 +69,64 @@ public class UserTest {
     }
 
     @Test
-    public void downVotingAnswerDecreasesOnePoint() {
+    public void downVotingAnswerDecreasesOnePoint() throws Exception {
         // User1 down-vote User2 answer
         user1.downVote(answer);
 
         // verify that User2's reputation decreases by 1 point
         assertEquals(-1, user2.getReputation());
+    }
+
+    // Authors Cannot Vote
+    @Test
+    public void userCannotUpvoteTheirOwnQuestion() throws Exception {
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        // User attempts to vote for their own question
+        user1.upVote(question);
+
+        // Verify that user can not vote for herself
+        assertFalse(true);
+    }
+
+    @Test
+    public void userCannotDownvoteTheirOwnQuestion() throws Exception {
+        // Verify that the appropriate exception is thrown with the correct message
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        // User attempts to down-vote for their own question
+        user1.downVote(question);
+
+        // Verify that user can not vote for herself
+        assertFalse(true);
+    }
+
+    @Test
+    public void userCannotUpvoteTheirOwnAnswer() throws Exception {
+        // Verify that the appropriate exception is thrown with the correct message
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        // User attempts to vote for their own answer
+        user2.upVote(answer);
+
+        // Verify that user can not vote for himself
+        assertFalse(true);
+    }
+
+
+    @Test
+    public void userCannotDownvoteTheirOwnAnswer() throws Exception {
+        // Verify that the appropriate exception is thrown with the correct message
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        // User attempts to down-vote for their own answer
+        user2.downVote(answer);
+
+        // Verify that user can not vote for himself
+        assertFalse(true);
     }
 }
